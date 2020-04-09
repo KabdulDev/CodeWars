92ec6249d9124e2cc367d499b999dc18931fece4
Karimu <Karimu.mohammed@gmail.com>
2020-04-08 22:41:40 -0400

    Subject: Slight Update to Description Tag

--- js/katas/level8/returnNegative.js
+++ js/katas/level8/returnNegative.js
@@ -1,7 +1,7 @@
 /* 
 Author: Karimu Mohammed
 Project Name: Return Negative
-Description: From HackerRank: "In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+Description: From CodeWars> "In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 Notes:
 
 The number can be negative already, in which case no change is required.
