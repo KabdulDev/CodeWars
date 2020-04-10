cec4ff0826fa5a760136b0ca678082ca79f216f5
Karimu <Karimu.mohammed@gmail.com>
2020-04-09 23:44:01 -0400

    Added interesting solutions to the code for review

--- js/katas/level8/smallestElement.js
+++ js/katas/level8/smallestElement.js
@@ -32,4 +32,50 @@ class SmallestIntegerFinder {
 const instance = new SmallestIntegerFinder
 console.log(instance.findSmallestInt([1,2,3,-6,9,-2])) 
 */
+
+//Dope Alternative CodeWars Solutions -Note to Self: All of the elegant solutions make it clear that it's necessary to review the Math library as well as the sort and reduce  methods
+/* 
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
+I like this because it's simple and to the point  gets the minimum from the array
+
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min.apply(null, args);
+  }
+}
+I like for some reasons as above, just alternate way to get there.
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    args.sort(function(a, b) {
+    return a - b; } )
+    return args[0];
+  }
+}
+
+I like this because of the use of the sort method to organize the elements and then just return the the element at 0. Also the function within the function is a useful view of the solution to potentiallly ape.
+
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.sort((a,b)=>a-b)[0];
+  }
+}
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.reduce(function(prev, curr, index, array) {
+      return prev < curr ? prev : curr;
+    });
+  }
+}
+
+
+*/
   
