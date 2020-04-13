ef9a96ca6ee30558052042d22860a191313ad08c
Karimu <Karimu.mohammed@gmail.com>
2020-04-13 10:47:31 -0400

    Subject: Adding comments on disemvowel novel solutions

--- js/katas/level7/disemvowelTrolls.js
+++ js/katas/level7/disemvowelTrolls.js
@@ -21,3 +21,28 @@ const disemvowel = str => str.replace(/[aeiou]/gi, "");
 
 //What I used for test case. Will eventually actually start creating proper tests internally.
 //console.log(disemvowel("The dog jumped over the moon"));
+
+/* 
+Dope Alternative Code Wars Solutions and Why I like them
+
+function disemvowel(str) {
+  return (str || "").replace(/[aeiou]/gi, "");
+}
+
+//I found the above response super interesting and was left with a question of "Why solve it this way". To get my answer your boy went to the Code Wars comments on this solution and reposted below.
+
+This is a short circuit operation, an OR specifically. You can have several conditions, this will ignore all 'falsy' conditions, and we use the first true condition. Example:
+
+This will assign 4 to the var f:
+
+var a; // undefined (falsy value)
+var b = null; // null (falsy value)
+var c = undefined; undefined (falsy value)
+var d = 4; // number (NOT falsy)
+var e = 'five'; // assigment short circuits before reaching here
+
+var f = a || b || c || d || e;
+
+So this solution checks first if str has something assignet to it (the str before ||), if it has nothing, it evaluates to a falsy (undefined) value, then javascript checks for '', which is an empty string but is not undefined (please double check this), executes the RegExp with this empty string and returns the value.
+
+*/
