b05f845e7e736858c3d91665000285d6efd52109
Karimu <Karimu.mohammed@gmail.com>
2020-04-28 21:53:01 -0400

    Subject: 3or5 logic finished and final edits included alongside novel solutions

--- js/katas/level6/3or5.js
+++ js/katas/level6/3or5.js
@@ -14,38 +14,34 @@ Date Created: 2020.04.27
 Date Modified: 2020.04.27
 */
 
-var multiples = [];
-  
-function multsOf3_5(number) {
-  if (number == 0) {
-      return multiples
-      //multiples.reduce();
-  } else {
-    if (number % 3 == 0) {
-      multiples.push(number);
-      multsOf3_5(number-1)
-    } else if (number % 5 == 0) {
-      multiples.push(number);
-      multsOf3_5(number-1)
-    }
-    else{
-        multsOf3_5(number-1)
-    }
-  
-  }
-}
+const solution = (number) =>
+  Array.from({ length: number }, (v, i) => i)
+    .filter((num) => num % 3 == 0 || num % 5 == 0)
+    .reduce((totalVal, currentVal) => totalVal + currentVal, 0);
 
-
-
-const solution = (number) => {
-  //Psuedo code time everybody. Let's talk this out
+/* 
+//Psuedo code time everybody. Let's talk this out
 
   //First get some function (or constant) solution that takes song as an input. -done
 
-  //Second find an array of all the positive multiples of 3 or 5 less than the number passed in a.k.a. create an array of items divisible by 3 or 5 bound by number and 3
-/*   let multiples = [];
+  //Second find an array of all the positive multiples of 3 or 5 less than the number passed in a.k.a. create an array of items divisible by 3 or 5 bound by number and 3 -- 
+  //On 2nd thought with a nigth of sleep, figured let's break this down further. Second - a) Make an array of items from 0 to BELOW THE number PASSED IN -- this got me first submittal
+
+  let multiples = Array.from({length: number}, (v, i) => i); // referenced from Stack Overflow that from would be helpful here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
   
-  function multsOf3_5(number) {
+
+  //Second -b) filter that given array to remove to contain only items divisible by 3 or 5;
+  let multsOf3_5 = multiples.filter( num => (num % 3 == 0) || (num % 5 == 0) );
+  
+
+  //Add all of the elements of the array to get the final sum. //Reviewing the test cases and submittal 
+  return multsOf3_5.reduce((totalVal, currentVal) => totalVal + currentVal);
+
+  //One liner version coming now
+
+   */
+
+/*  function multsOf3_5(number) {
     if (number == 0) {
         return multiples
         //multiples.reduce();
@@ -60,20 +56,35 @@ const solution = (number) => {
   }
   multsOf3_5(number) */
 
-  //Reduce that array with addition to get final result
-  
-};
+//Reduce that array with addition to get final result
+
 /* 
 Dope Alternative Code Wars Solutions and Why I like them
 
+function solution(number){
+  var sum = 0;
+  
+  for(var i = 1;i< number; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+      sum += i
+    }
+  }
+  return sum;
+}
+//This solution is a nice return of the for loop gang.
 
-//This solution is just smooth and smart as all hell. Need to get my trim game up.
+
+function solution(number){
+  var n3 = Math.floor(--number/3), n5 = Math.floor(number/5), n15 = Math.floor(number/15);
+  return (3 * n3 * (n3 + 1) + 5 * n5 * (n5 + 1) - 15 * n15 * (n15+1)) /2;
+}
+//Interested in their use of the Math methods, even though I don't currently understand them.
 
 
-
-//Super similar to the answer i thought, but their filter is simpler because empty strings evaluate to false, thus Boolean works just fine. Super smart and worth remembering.
+solution= n=> n<=0?0:Array.from({length: n}, (_,i) => (i%3==0||i%5==0)?i:0).reduce((x,y)=>x+y)
+//Cleaner version of my solution. I like how they chose to solve for the 0 case. This one is syntactically interesting
 
 
 */
 
-console.log(multsOf3_5(23));
+console.log(solution(0));
